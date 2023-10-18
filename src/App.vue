13b92888aa628dc717de496f4dc017dc20dd01d0
RafaelBgbg <140459802+RafaelBgbg@users.noreply.github.com>
2023-10-18 16:39:19 -0300

    atualização

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ const loading = ref(false);
 const loadingPosts = ref(false);
 const message = ref(null);
 
-const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const contractAddress = "0x397d7231Ca1e807594Ba1A1235E2B7C8EDFd67aD";
 const contractABI = abi.abi;
 
 const getEthereumObject = () => window.ethereum;
